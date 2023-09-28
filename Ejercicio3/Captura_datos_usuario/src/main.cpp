79852f37e114b1544ee219a3b75b58973b603d85
SISTRAN/cbarajas <cbarajas@sistran.com.co>
2023-09-27 20:07:41 -0500

    Se agrega una clase para la captura de datos enteros

--- Ejercicio3/Captura_datos_usuario/src/main.cpp
+++ Ejercicio3/Captura_datos_usuario/src/main.cpp
@@ -1,20 +1,14 @@
 #include <Arduino.h>
+#include <DataCapture.h>
 
 void setup() {
   Serial.begin(9600);
   Serial.println();
 
-  Serial.println("Digite un número: ");
-  while (!Serial.available()) {}
-  int number = Serial.parseInt();
-  Serial.println("Primer número: " + String(number));
+  int number = DataCapture::captureInteger("Ingrese primer número:");
+  int OtherNumber = DataCapture::captureInteger("Ingrese segundo número:");
 
-  Serial.println("Digite otro número: ");
-  while (!Serial.available()) {}
-  int OtherNumber = Serial.parseInt();
-  Serial.println("Segundo número: " + String(OtherNumber));
-
-  Serial.println("Suma: " + String(number + OtherNumber));
+  Serial.println("Resultado de la Suma: " + String(number + OtherNumber));
 }
 
 void loop() {

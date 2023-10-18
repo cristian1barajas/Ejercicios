ea7fba58220c248b5f926c2ca7a5cb5450e1fe38
SISTRAN/cbarajas <cbarajas@sistran.com.co>
2023-10-17 20:32:27 -0500

    Implementación de la libreria actualizado

--- Ejercicio3/Captura_datos_usuario/src/main.cpp
+++ Ejercicio3/Captura_datos_usuario/src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <DataCapture.h>
 
+using namespace DataCapture;
+
 void setup() {
   Serial.begin(9600);
   Serial.println();
 
-  int number = DataCapture::captureInteger("Ingrese primer número:");
-  int OtherNumber = DataCapture::captureInteger("Ingrese segundo número:");
+  int number = captureInteger("Ingrese primer número:");
+  int OtherNumber = captureInteger("Ingrese segundo número:");
 
   Serial.println("Resultado de la Suma: " + String(number + OtherNumber));
 
